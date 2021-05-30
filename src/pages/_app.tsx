54988e64014f9073d5841e0a08f93ce1960ecc3c
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-05-30 14:47:43 -0300

    feat: add react-toastify

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,17 @@
 import { AppProps } from 'next/app';
 
+import { ToastContainer } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <ToastContainer />
+    </>
+  );
 }
 
 export default MyApp;
--- src/pages/import.tsx
+++ src/pages/import.tsx
@@ -1,4 +1,7 @@
+import { useState } from 'react';
+
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
 import Footer from '../components/Footer';
 import Header from '../components/Header';
@@ -9,15 +12,59 @@ interface ImportProps {
   baseUrl: string;
 }
 
-export default function Import({ baseUrl }: ImportProps) {
-  const handleUpload = (files: File[]) => {
-    try {
-      const data = new FormData();
-      data.append('file', files[0], files[0].name);
+interface Company {
+  name: string;
+  cnpj: string;
+}
 
-      axios.post(`${baseUrl}/xml/import`, data);
+interface Product {
+  id: number;
+  name: string;
+  total_price: number;
+  unit_price: number;
+}
+
+interface Total {
+  discount: number;
+  icms_st: number;
+  ipi: number;
+  nf: number;
+  others: number;
+  products: number;
+  safe: number;
+  shipping: number;
+}
+
+interface Note {
+  number: number;
+  seller: Company;
+  customer: Company;
+  products: Product[];
+  total: Total;
+}
+
+export default function Import({ baseUrl }: ImportProps) {
+  const [note, setNote] = useState<Note>({} as Note);
+  const [showInput, setShowInput] = useState(true);
+  const [file, setFile] = useState(null);
+
+  const handleUpload = (files: File[]) => {
+    setFile(files[0]);
+  };
+
+  const handleSubmit = () => {
+    try {
+      setShowInput(false);
+
+      const data = new FormData();
+      data.append('file', file, file.name);
+
+      axios
+        .post<Note>(`${baseUrl}/xml/import`, data)
+        .then((response) => setNote(response.data));
     } catch (err) {
       console.error(err);
+      toast.error('Ocorreu um erro tente novamente ou contate o desenvolvedor');
     }
   };
 
@@ -26,7 +73,16 @@ export default function Import({ baseUrl }: ImportProps) {
       <Header />
 
       <main className={styles.container}>
-        <Upload onUpload={handleUpload} />
+        {showInput && <Upload onUpload={handleUpload} />}
+
+        <div className={styles.button_container}>
+          <button type="button" onClick={() => setShowInput(true)}>
+            Novo upload
+          </button>
+          <button type="button" onClick={handleSubmit}>
+            Calcular preço
+          </button>
+        </div>
       </main>
 
       <Footer />
