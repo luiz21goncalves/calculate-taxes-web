691e01d96950aad85b57c3cbac622896f1dfc751
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-05-30 09:59:09 -0300

    feat: add global style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
 import { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps } : AppProps) {
-  return <Component {...pageProps} />
+import '../styles/global.scss';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
