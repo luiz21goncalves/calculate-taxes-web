29405354281428cfd6b3dbd3b0839025eede952f
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-07-29 18:31:19 -0300

    feat: add chakra-ui config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,17 @@
 import { AppProps } from 'next/app';
 
 import { ToastContainer } from 'react-toastify';
+import { ChakraProvider } from '@chakra-ui/react';
 
-import { GlobalStyle } from '../styles/global';
+import { theme } from '../styles/theme';
 import 'react-toastify/dist/ReactToastify.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
       <ToastContainer />
-      <GlobalStyle />
-    </>
+    </ChakraProvider>
   );
 }
 
