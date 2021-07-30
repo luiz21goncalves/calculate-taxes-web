97216d45db304ea538f8b79d397efa93efbe9e38
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-07-29 21:24:27 -0300

    refactor: add chakra-ui in Table component

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,12 +1,29 @@
 import { useEffect, useState } from 'react';
 
+import {
+  Button,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverCloseButton,
+  PopoverContent,
+  PopoverHeader,
+  PopoverTrigger,
+  Table as ChakraTable,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react';
+
 import { formattedPrice } from '../../utils';
-import { Container, ModalContainer } from './styles';
-import Modal from '../Modal';
 
 interface FormattedProduct {
   id: string;
   name: string;
+  unit: string;
   total_price: number;
   unit_price: number;
   quantity: number;
@@ -16,6 +33,7 @@ export interface Product {
   id: string;
   name: string;
   quantity: number;
+  unit: string;
   unit_price: number;
   total_price: number;
   taxes: {
@@ -30,6 +48,7 @@ export interface Product {
 interface ShowProduct {
   id: string;
   name: string;
+  unit: string;
   quantity: number;
   unit_price: number;
   total_price: number;
@@ -52,7 +71,6 @@ export default function Table({ products }: TableProps) {
   const [formattedProducts, setFormattedProducts] = useState<
     FormattedProduct[]
   >([]);
-  const [modalIsOpen, setModalIsOpen] = useState(false);
   const [showProduct, setShowProduct] = useState<ShowProduct>(
     {} as ShowProduct,
   );
@@ -70,12 +88,6 @@ export default function Table({ products }: TableProps) {
     product.total_price_with_taxes = formattedProduct.total_price;
 
     setShowProduct(product);
-
-    toggleModal();
-  }
-
-  function toggleModal() {
-    setModalIsOpen((state) => !state);
   }
 
   useEffect(() => {
@@ -89,6 +101,7 @@ export default function Table({ products }: TableProps) {
         id,
         name,
         shipping,
+        unit,
       } = product;
 
       const totalPrice =
@@ -98,6 +111,7 @@ export default function Table({ products }: TableProps) {
         quantity,
         id,
         name,
+        unit,
         total_price: totalPrice,
         unit_price: totalPrice / (quantity / 100),
       });
@@ -109,106 +123,114 @@ export default function Table({ products }: TableProps) {
   }, [products]);
 
   return (
-    <Container>
-      <thead>
-        <tr>
-          <th>código</th>
-          <th>nome</th>
-          <th>quantidate</th>
-          <th>preço unitario</th>
-          <th>preço total</th>
-        </tr>
-      </thead>
+    <ChakraTable colorScheme="black">
+      <Thead border="1px" borderColor="blue.900">
+        <Tr>
+          <Th bg="blue.900" borderRight="1px" borderRightColor="gray.50">
+            <Text fontSize="md" color="gray.50" textAlign="center">
+              código
+            </Text>
+          </Th>
+          <Th bg="blue.900" borderRight="1px" borderRightColor="gray.50">
+            <Text fontSize="md" color="gray.50" textAlign="center">
+              nome
+            </Text>
+          </Th>
+          <Th bg="blue.900" borderRight="1px" borderRightColor="gray.50">
+            <Text fontSize="md" color="gray.50" textAlign="center">
+              unidade
+            </Text>
+          </Th>
+          <Th bg="blue.900" borderRight="1px" borderRightColor="gray.50">
+            <Text fontSize="md" color="gray.50" textAlign="center">
+              quantidate
+            </Text>
+          </Th>
+          <Th bg="blue.900" borderRight="1px" borderRightColor="gray.50">
+            <Text fontSize="md" color="gray.50" textAlign="center">
+              preço unitario
+            </Text>
+          </Th>
+          <Th bg="blue.900">
+            <Text fontSize="md" color="gray.50" textAlign="center">
+              preço total
+            </Text>
+          </Th>
+        </Tr>
+      </Thead>
 
-      <tbody>
+      <Tbody>
         {formattedProducts.map(
-          ({ id, name, quantity, total_price, unit_price }) => (
-            <tr key={id}>
-              <Modal setIsOpen={toggleModal} isOpen={modalIsOpen}>
-                <ModalContainer>
-                  <div>
-                    <strong>Código: </strong>
-                    <span>{showProduct?.id}</span>
-                  </div>
-
-                  <div>
-                    <strong>Nome: </strong>
-                    <span>{showProduct?.name}</span>
-                  </div>
-
-                  <div>
-                    <strong>Quantidade: </strong>
-                    <span>{showProduct?.quantity / 100}</span>
-                  </div>
-
-                  <div>
-                    <strong>Valor unitário: </strong>
-                    <span>{formattedPrice(showProduct?.unit_price)}</span>
-                  </div>
-
-                  <div>
-                    <strong>Valor total do produto: </strong>
-                    <span>{formattedPrice(showProduct?.total_price)}</span>
-                  </div>
-
-                  <div>
-                    <strong>ICMS ST: </strong>
-                    <span>{formattedPrice(showProduct?.taxes?.icms_st)}</span>
-                  </div>
-
-                  <div>
-                    <strong>IPI: </strong>
-                    <span>{formattedPrice(showProduct?.taxes?.ipi)}</span>
-                  </div>
-
-                  <div>
-                    <strong>Outras despesas: </strong>
-                    <span>{formattedPrice(showProduct?.other)}</span>
-                  </div>
-
-                  <div>
-                    <strong>Frete: </strong>
-                    <span>{formattedPrice(showProduct?.shipping)}</span>
-                  </div>
-
-                  <div>
-                    <strong>Desconto: </strong>
-                    <span>{formattedPrice(showProduct?.discount)}</span>
-                  </div>
-
-                  <div>
-                    <strong>Valor unitário com impostos: </strong>
-                    <span>
-                      {formattedPrice(showProduct?.unit_price_with_taxes)}
-                    </span>
-                  </div>
-
-                  <div>
-                    <strong>Valor total com impostos: </strong>
-                    <span>
-                      {formattedPrice(showProduct.total_price_with_taxes)}
-                    </span>
-                  </div>
-                </ModalContainer>
-              </Modal>
-
-              <td>{id}</td>
-              <td>{name}</td>
-              <td>{quantity / 100}</td>
-              <td>{formattedPrice(unit_price)}</td>
-              <td>
-                <button type="button" onClick={() => handleShowProduct(id)}>
-                  {formattedPrice(total_price)}
-                </button>
-
-                <div>
-                  <span>cliqui para ver mais detalhes</span>
-                </div>
-              </td>
-            </tr>
+          ({ id, name, quantity, total_price, unit_price, unit }) => (
+            <Tr key={id}>
+              <Td border="1px" borderColor="black">
+                <Text textAlign="center">{id}</Text>
+              </Td>
+              <Td borderRight="1px" borderRightColor="black">
+                <Text textAlign="center">{name}</Text>
+              </Td>
+              <Td borderRight="1px" borderRightColor="black">
+                <Text textAlign="center" textTransform="uppercase">
+                  {unit}
+                </Text>
+              </Td>
+              <Td borderRight="1px" borderRightColor="black">
+                <Text textAlign="center">{quantity / 100}</Text>
+              </Td>
+              <Td borderRight="1px" borderRightColor="black">
+                <Text textAlign="center">{formattedPrice(unit_price)}</Text>
+              </Td>
+              <Td borderRight="1px" borderRightColor="black">
+                <Popover>
+                  <PopoverTrigger>
+                    <Button
+                      variant="outline"
+                      fontWeight="bold"
+                      textAlign="center"
+                      w="100%"
+                      h="100%"
+                      fontSize="16"
+                      p="1"
+                      colorScheme="gray"
+                      onClick={() => handleShowProduct(id)}
+                    >
+                      {formattedPrice(total_price)}
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent>
+                    <PopoverArrow />
+                    <PopoverCloseButton />
+                    <PopoverHeader textAlign="center">
+                      Detalhe do calculo
+                    </PopoverHeader>
+                    <PopoverBody>
+                      <Text fontSize="sm" textTransform="uppercase">
+                        preço unitario:{' '}
+                        {formattedPrice(showProduct?.unit_price)}
+                      </Text>
+                      <Text fontSize="sm" textTransform="uppercase">
+                        icms st: {formattedPrice(showProduct?.taxes?.icms_st)}
+                      </Text>
+                      <Text fontSize="sm" textTransform="uppercase">
+                        ipi: {formattedPrice(showProduct?.taxes?.ipi)}
+                      </Text>
+                      <Text fontSize="sm" textTransform="uppercase">
+                        outros: {formattedPrice(showProduct?.other)}
+                      </Text>
+                      <Text fontSize="sm" textTransform="uppercase">
+                        frete: {formattedPrice(showProduct?.shipping)}
+                      </Text>
+                      <Text fontSize="sm" textTransform="uppercase">
+                        preço total: {formattedPrice(showProduct?.total_price)}
+                      </Text>
+                    </PopoverBody>
+                  </PopoverContent>
+                </Popover>
+              </Td>
+            </Tr>
           ),
         )}
-      </tbody>
-    </Container>
+      </Tbody>
+    </ChakraTable>
   );
 }
