22f873b3f030b9621eb147c313ef95d6365c4460
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-10-28 21:22:07 -0300

    chore: add functions to convert minutes and hours

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,20 @@ export const formattedPrice = (value: number) =>
     style: 'currency',
     currency: 'BRL',
   }).format(value / 100);
+
+export function convertHoursInMinutes(time: string) {
+  const [hours, minutes] = time.split(':');
+
+  return Number(hours) * 60 + Number(minutes);
+}
+
+export function convertMinutesInHours(time: number) {
+  const convertedMinutes = time % 60;
+  const convertedHour = Math.floor(time / 60);
+
+  const formatedHour = convertedHour > 10 ? convertedHour : `0${convertedHour}`;
+  const formetedMinute =
+    convertedMinutes > 10 ? convertedMinutes : `0${convertedMinutes}`;
+
+  return `${formatedHour}:${formetedMinute}`;
+}
