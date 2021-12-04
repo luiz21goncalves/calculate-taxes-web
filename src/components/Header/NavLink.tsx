d113301d41db630a8b6194b47bf807bb64130a6b
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-12-04 14:35:40 -0300

    chore: add NavLink component

---
+++ src/components/Header/NavLink.tsx
@@ -0,0 +1,20 @@
+import { Link } from '@chakra-ui/react';
+
+import { ActiveLink } from './ActiveLink';
+
+type NavLinkProps = {
+  children: string;
+  href: string;
+};
+
+export function NavLink(props: NavLinkProps) {
+  const { children, href } = props;
+
+  return (
+    <ActiveLink href={href} passHref>
+      <Link fontSize="lg" fontWeight="bold" _hover={{ opacity: 0.6 }}>
+        {children}
+      </Link>
+    </ActiveLink>
+  );
+}
