ca562d9fef441031f3bd23028e2cd60ef5dd7ed3
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-12-04 14:35:57 -0300

    chore: change max width of nav

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,51 +1,25 @@
-import Link from 'next/link';
+import { Box, Stack } from '@chakra-ui/react';
 
-import { Box, Flex, Stack, Link as ChakraLink, Text } from '@chakra-ui/react';
+import { NavLink } from './NavLink';
 
 export default function Header() {
   return (
     <Box as="header" bg="blue.900" h="20" shadow="xl">
-      <Flex
+      <Stack
+        as="nav"
         direction="row"
-        justify="space-between"
+        spacing="8"
         align="center"
-        maxW={1280}
+        justify="flex-start"
+        maxW="container.lg"
         px="4"
         mx="auto"
         h="100%"
       >
-        <Stack as="nav" direction="row" spacing="8" align="center">
-          <Link href="/import" passHref>
-            <ChakraLink
-              color="gray.200"
-              fontSize="18"
-              _hover={{ color: 'whiteAlpha.600' }}
-            >
-              <Text>Importar XML</Text>
-            </ChakraLink>
-          </Link>
-
-          <Link href="/" passHref>
-            <ChakraLink
-              color="gray.200"
-              fontSize="18"
-              _hover={{ color: 'whiteAlpha.600' }}
-            >
-              <Text>Sobre o projeto</Text>
-            </ChakraLink>
-          </Link>
-        </Stack>
-
-        <ChakraLink
-          href="https://github.com/luiz21goncalves"
-          color="gray.200"
-          isExternal
-          fontSize="18"
-          _hover={{ color: 'blue.200' }}
-        >
-          Desenvolvido por Luiz Gonçalves
-        </ChakraLink>
-      </Flex>
+        <NavLink href="/">Home</NavLink>
+        <NavLink href="/import">Importar XML</NavLink>
+        <NavLink href="/timesheet">Calcular horas extras</NavLink>
+      </Stack>
     </Box>
   );
 }
