19958892c8ea95bfe196ca043fb0062896dbd8ce
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-05-22 21:36:41 -0300

    chore: run lint

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,12 +1,12 @@
-import type { V2_MetaFunction } from "@remix-run/node";
+import type { V2_MetaFunction } from '@remix-run/node'
 
 export const meta: V2_MetaFunction = () => {
-  return [{ title: "New Remix App" }];
-};
+  return [{ title: 'New Remix App' }]
+}
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
       <h1>Welcome to Remix</h1>
       <ul>
         <li>
@@ -34,5 +34,5 @@ export default function Index() {
         </li>
       </ul>
     </div>
-  );
+  )
 }
