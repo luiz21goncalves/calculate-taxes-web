4e0325da3787d1f08155325afcf2a4de173eddf0
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-05-22 21:36:20 -0300

    style: add rocketseat eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,27 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
-};
+  extends: [
+    '@remix-run/eslint-config',
+    '@remix-run/eslint-config/node',
+    '@rocketseat/eslint-config/react',
+  ],
+  plugins: ['eslint-plugin-import-helpers'],
+  rules: {
+    camelcase: 'off',
+    'react/self-closing-comp': 'error',
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always',
+        groups: [
+          '/^node:/',
+          ['/^@remix-run/', '/^react/'],
+          'module',
+          '/~//',
+          ['parent', 'sibling', 'index'],
+        ],
+        alphabetize: { order: 'asc', ignoreCase: true },
+      },
+    ],
+  },
+}
