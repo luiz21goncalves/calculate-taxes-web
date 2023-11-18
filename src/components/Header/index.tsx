6250172350411165d8080c51501c46be0e5689b1
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-11-18 13:06:29 -0300

    feat: add header padding & increase gap

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import { NavLink } from './nav-link'
 export function Header() {
   return (
     <header className="flex h-20 w-full items-center justify-center bg-blue-950">
-      <nav className="flex w-full max-w-5xl gap-4">
+      <nav className="flex w-full max-w-5xl gap-8 p-4">
         <NavLink href="/">Home</NavLink>
         <NavLink href="/import" exactMatch={false}>
           Importar XML
