9ab7bf092c7350cfd5ae068c24dbbf92ddbb2c24
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-11-18 12:36:55 -0300

    fix: active links for nested routes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,9 @@ export function Header() {
     <header className="flex h-20 w-full items-center justify-center bg-blue-950">
       <nav className="flex w-full max-w-5xl gap-4">
         <NavLink href="/">Home</NavLink>
-        <NavLink href="/import">Importar XML</NavLink>
+        <NavLink href="/import" exactMatch={false}>
+          Importar XML
+        </NavLink>
         <NavLink href="/timesheet">Calcular horas extras</NavLink>
       </nav>
     </header>
